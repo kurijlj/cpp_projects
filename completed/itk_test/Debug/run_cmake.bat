7c829aad2a603e28735ef5d6ac476cc7a6a61a55
Ljubomir Kurij <ljubomir_kurij@protonmail.com>
2021-03-24 21:22:21 +0100

    GreatOtter Lap: Added run_cmake script

---
+++ completed/itk_test/Debug/run_cmake.bat
@@ -0,0 +1,7 @@
+@REM Put comments like this
+
+set BUILD_TYPE=Debug
+set Path=C:\ITK\build\5.2\bin;%Path%
+
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+call cmake -DBUILD_SHARED_LIBS:BOOL=ON -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_STANDARD=17 -DCMAKE_CXX_STANDARD_REQUIRED:BOOL=ON -DITK_DIR=C:\ITK\build -G "NMake Makefiles" ..
