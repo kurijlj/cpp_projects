975f37f458240e993cc21e5fc5de1d0ee745fca2
Ljubomir Kurij <ljubomir_kurij@protonmail.com>
2021-07-21 22:23:10 +0200

    GreatOtter Desk: Basic library interface implemented with file open and close facilities

--- work_in_progress/tiffio++/include/tiffio++/tiffio++.hpp
+++ work_in_progress/tiffio++/include/tiffio++/tiffio++.hpp
@@ -103,6 +103,7 @@ public:
     class TIFFIOException {
     protected:
         std::string message_;
+
     public:
         TIFFIOException() : message_("") {}
         TIFFIOException(std::string message) : message_(message) {}
@@ -142,28 +143,100 @@ public:
 
     ///////////////////////////////////////////////////////////////////////////
     //
-    // File Access Flags  - libtiff distincts fthree types of file access mode:
+    // File Access Flags  - from the libtiff man pages:
+    // libtiff distincts three types of file access mode:
     //
     //     a  - append
     //     r  - read
     //     w  - write
     //
+    // The open mode parameter can include the following flags in addition to
+    // the "r", "w", and "a" flags. Note however that option flags must follow
+    // the read-write-append specification.
+    //
+    //     l  - When creating a new file force information be written with
+    //          Little-Endian byte order (but see below). By default the library
+    //          will create new files using the native CPU byte order.
+    //     b  - When creating a new file force information be written with
+    //          Big-Endian byte order (but see below). By default the library
+    //          will create new files using the native CPU byte order.
+    //     L  - Force image data that is read or written to be treated with bits
+    //          filled from Least Significant Bit ( LSB ) to Most Significant
+    //          Bit ( MSB ). Note that this is the opposite to the way the
+    //          library has worked from its inception.
+    //     B  - Force image data that is read or written to be treated with bits
+    //          filled from Most Significant Bit ( MSB ) to Least Significant
+    //          Bit ( LSB ); this is the default.
+    //     H  - Force image data that is read or written to be treated with bits
+    //          filled in the same order as the native CPU.
+    //     M  - Enable the use of memory-mapped files for images opened
+    //          read-only. If the underlying system does not support
+    //          memory-mapped files or if the specific image being opened
+    //          cannot be memory-mapped then the library will fallback to using
+    //          the normal system interface for reading information. By default
+    //          the library will attempt to use memory-mapped files.
+    //     m  - Disable the use of memory-mapped files.
+    //     C  - Enable the use of ``strip chopping'' when reading images that
+    //          are comprised of a single strip or tile of uncompressed data.
+    //          Strip chopping is a mechanism by which the library will
+    //          automatically convert the single-strip image to multiple strips,
+    //          each of which has about 8 Kilobytes of data. This facility can
+    //          be useful in reducing the amount of memory used to read an image
+    //          because the library normally reads each strip in its entirety.
+    //          Strip chopping does however alter the apparent contents of the
+    //          image because when an image is divided into multiple strips it
+    //          looks as though the underlying file contains multiple separate
+    //          strips. Finally, note that default handling of strip chopping
+    //          is a compile-time configuration parameter. The default
+    //          behaviour, for backwards compatibility, is to enable
+    //          strip chopping.
+    //     c  - Disable the use of strip chopping when reading images.
+    //
     ///////////////////////////////////////////////////////////////////////////
     class FileAccessMode {
-    protected:
-        std::string value_;
     public:
-        FileAccessMode() : value_("none") {}
-        FileAccessMode(std::string mode) : value_(mode) {}
-        FileAccessMode(const FileAccessMode &inst) : value_(inst.value()) {}
+        enum MajorModeParam {Append, Read, Write};
+        enum MinorModeParam {
+            ForceLittleEndian,
+            ForceBigEndian,
+            ForceLSB,
+            ForceMSB,
+            ForceNativeCPUOrder,
+            EnableMemoryMappedFiles,
+            DisableMemoryMappedFiles,
+            EnableStripChopping,
+            DisableStripChopping,
+            None
+        };
+
+    private:
+        MajorModeParam major_mode_;
+        MinorModeParam minor_mode_;
+
+    public:
+        FileAccessMode()
+            : major_mode_(MajorModeParam::Read),
+              minor_mode_(MinorModeParam::None) {}
+        FileAccessMode(MajorModeParam major_mode, MinorModeParam minor_mode)
+            : major_mode_(major_mode),
+              minor_mode_(minor_mode) {}
+        FileAccessMode(const FileAccessMode &inst)
+            : major_mode_(major_mode),
+              minor_mode_(minor_mode) {}
+            : value_(inst.value()) {}
         ~FileAccessMode() {}
 
         // Methods
-        const char* c_str() { return value_.c_str(); }
+        const char* c_str();
+
         bool equal_to(FileAccessMode other) const {
-            return (other.value() == value_ ? true : false);
+            return ((
+                    (other.majorMode() == major_mode_)
+                    && (other.minorMode() == minor_mode_)
+                    ) ? true : false);
         }
-        std::string value() const { return value_; }
+        MajorModeParam majorMode() const { return major_mode_; }
+        MajorModeParam minorMode() const { return minor_mode_; }
 
         // Operators
         bool operator==(FileAccessMode other) { return equal_to(other); }
@@ -175,7 +248,7 @@ private:
     TIFFErrorHandler old_error_handler_, old_warning_handler_;
     bool file_opened_, print_errors_, print_warnings_;
     std::string file_name_;
-    // AccessMode access_mode_;
+    FileAccessMode access_mode_;
 
     void errorHandler(const char* module, const char* fmt, ...);
     void restoreHandlers();
@@ -185,7 +258,10 @@ private:
 public:
     // Constructors
     TOFFIOObject();
-    TOFFIOObject(const std::string file_name);
+    TOFFIOObject(
+            const std::string file_name,
+            const FileAccessMode access_mode
+            );
 
     // Destructors
     ~TIFFIOObject() {}
@@ -218,9 +294,50 @@ static TIFFIOObject* pointer_to_instance;
 
 
 // ============================================================================
-// TIFFIOObject class definitions
+// TIFFIOObject::FileAccessMode class definitions
 // ============================================================================
 
+// ============================================================================
+// TIFFIOObject::FileAccessMode public methods definitions
+// ============================================================================
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Returns value of file access mode paramter constuction as 'C'string for
+// passing to the native libtiff calls (i.e. TIFFOpen())
+//
+///////////////////////////////////////////////////////////////////////////////
+
+const char* TIFFIOObject::FileAccessMode::c_str()
+{
+    std::string value;
+
+    switch(major_mode_) {
+        case Append: value.push_back('a'); break;
+        case Read: value.push_back('r'); break;
+        default: value.push_back('w');
+    }
+
+    switch(minor_mode_) {
+        case ForceLittleEndian: value.push_back('l'); break;
+        case ForceBigEndian: value.push_back('b'); break;
+        case ForceLSB: value.push_back('L'); break;
+        case ForceMSB: value.push_back('B'); break;
+        case ForceNativeCPUOrder: value.push_back('H'); break;
+        case EnableMemoryMappedFiles: value.push_back('M'); break;
+        case DisableMemoryMappedFiles: value.push_back('m'); break;
+        case EnableStripChopping: value.push_back('C'); break;
+        case DisableStripChopping: value.push_back('c'); break;
+        default: break;  // By default ('None') we don't add anything
+    }
+
+    return value.c_str();
+}
+
+
+// ============================================================================
+// TIFFIOObject class definitions
+// ============================================================================
 
 // ============================================================================
 // TIFFIOObject private methods definitions
@@ -374,7 +491,7 @@ void TIFFIOObject::warning_handler(
 
 TIFFIOObject::TIFFIOObject()
 {
-    // file_access_mode_ = TIFFIOObject::ReadMode();
+    access_mode_ = TIFFIOObject::FileAccessMode();
     file_name_ = "";
     file_opened_ = false;
     print_errors_ = true;
@@ -391,10 +508,10 @@ TIFFIOObject::TIFFIOObject()
 
 TIFFIOObject::TIFFIOObject(
         const std::string file_name
-        // const FileAccessMode file_access_mode = TIFFIOObject::ReadMode()
+        const FileAccessMode access_mode = TIFFIOObject::FileAccessMode()
         )
 {
-    // file_access_mode_ = file_access_mode;
+    access_mode_ = access_mode;
     file_name_ = file_name;
     file_opened_ = false;
     print_errors_ = false;
@@ -477,7 +594,7 @@ bool TIFFIOObject::open()
     if(!file_opened_) {
         save_handlers();
         // tiff_handle_ = TIFFOpen(file_name_.c_str(), file_access_mode_.c_str());
-        tiff_handle_ = TIFFOpen(file_name_.c_str(), file_access_mode_.c_str());
+        tiff_handle_ = TIFFOpen(file_name_.c_str(), access_mode_.c_str());
         restore_handlers();
     }
 
